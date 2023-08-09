ad5920474ce7a7395ea75ae927370c7cf70ba07a
Mynuddin-dev <mdmynuddin.iit.se.nstu@gmail.com>
2023-08-09 08:15:27 +0600

    What is OOP

---
+++ 13. OOP/00. What_is_OOP.js
@@ -0,0 +1,39 @@
+/*
+Getting know about OOP let we know how the developers write code before OOP comes?
+
+They follow procedural way rather than any structural way to write code.
+Procedural Programing :
+-----------------------
+In procedural programming, the focus is on writing procedures or functions that perform specific tasks. These procedures manipulate data and are organized in a linear fashion. The program's logic revolves around executing these procedures one after another, with data often shared globally.
+
+How Code Was Written in Procedural Programming:
+-----------------------------------------------
+1. Data and Function: Data and functions are separate entities. Functions act on data and can modify it, leading to a global data space where data is accessible from anywhere in the program
+2. Global Scope: lead unintended side effect and make the ode harder to understand and maintain.
+3.Complexity: As programs grew larger and more complex, managing data and ensuring its integrity became challenging. Code could become difficult to maintain and debug due to its sprawling nature.
+
+Then first comes STRUCT:Common data in a struct and we can create many user object as we can. And there is also many disadvantages.
+
+There is some serious issue we face when we create software for real world.
+1. Code is not reusable.
+2. Large code is hard to manage.
+3. Difficult to tracing and fixing bugs.
+4. Data is exposed to whole program(global scope)
+5. Here, operation's priority is higher than data
+6. Difficult to relate with real world data
+
+So what's the solution ? : OOP
+------------------------------
+Programming is like giving instructions to a computer to perform tasks. As your programs become more complex, organizing and managing your code becomes important. That's where Object-Oriented Programming (OOP) comes in!
+
+This is a way of writing computer programs by organizing your code into reusable pieces called "objects." These objects represent real-world things or concepts, and they have both data (attributes) and behaviors (methods). OOP allows you to model and solve complex problems in a more structured and organized manner.
+ 
+Class: A class is like a blueprint or template for creating objects. It defines the properties (attributes) and behaviors (methods) that objects of that class will have. For example, if you have a class called "Car," it might have attributes like "color" and "model," and methods like "start_engine."
+
+Object: An object is an instance created from a class. If a class is a blueprint, an object is a specific realization of that blueprint. You can create multiple objects from the same class, each with its own unique data.
+
+Attributes: Attributes are the characteristics or properties of an object. For instance, a "Car" object might have attributes like "color," "model," and "year."
+
+Methods: Methods are the functions or actions that an object can perform. For our "Car" example, methods could include "start_engine," "accelerate," and "brake."
+
+*/
